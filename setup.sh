66751147c49a877bdeffb4d02e0ac9398011235f
Heechul Yun <heechul@illinois.edu>
2017-05-28 15:52:53 -0500

    www page addition

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ roscore &
 sleep 5
 roslaunch rtimulib_ros rtimulib_ros.launch &  # publish 'imu' channel
 rosrun teleop_twist_joy teleop_node &         # publish 'cmd_vel' output from joystick input
-./src/servod &              # enable pwm output for the speed motor
+./servod &              # enable pwm output for the speed motor
 ./src/motor_driver.py & # subscribe 'cmd_vel' channel
 # rosrun teleop_twist_keyboard teleop_twist_keyboard.py &
