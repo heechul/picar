353f70ad6b31efe49c3de2d7e2243b98b9b794a2
Heechul Yun <heechul@illinois.edu>
2017-05-28 14:59:56 -0500

    ...

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ roscore &
 sleep 5
 roslaunch rtimulib_ros rtimulib_ros.launch &  # publish 'imu' channel
 rosrun teleop_twist_joy teleop_node &         # publish 'cmd_vel' output from joystick input
-/home/heechul/PiCar2016/servod &              # enable pwm output for the speed motor
-/home/heechul/PiCar2016/src/motor_driver.py & # subscribe 'cmd_vel' channel
+./src/servod &              # enable pwm output for the speed motor
+./src/motor_driver.py & # subscribe 'cmd_vel' channel
 # rosrun teleop_twist_keyboard teleop_twist_keyboard.py &
