f5c5973c6819564f1d72b1a9580fe4e60585fb2e
James Muoghalu <jmuoghal@dhcp233.ittc.ku.edu>
2017-06-20 13:55:17 -0500

    Using a tf Broadcaster to Move the Car in RViz

--- src/car_model/src/car_publisher.py
+++ src/car_model/src/car_publisher.py
@@ -2,41 +2,54 @@
 
 import roslib; roslib.load_manifest('car_model')
 import rospy, tf
-from geometry_msgs.msg import PoseStamped, TransformStamped, Twist
-from sensor_msgs.msg import JointState
 from std_msgs.msg import Header
+from geometry_msgs.msg import Point, PoseStamped, TransformStamped, Twist
+from sensor_msgs.msg import JointState
 
 
 joint_state = None
 transf = None
 pose_st = None
 joint_pub = None
-pose_pub = None
 broadcaster = None
+
+original_linx = 0.5
+previous_linx = 0.5
+current_linx = 0.5
+turn = 1
+angle = 1
+
+
+f_left = 0
+f_right = 0
+back_wheels = 0
+
+move_x = 0
+move_y = 0
+move_z = 0
+
 
 
 def receive_twist(msg):
-    rospy.loginfo("Received a /cmd_vel message!")
-    rospy.loginfo("Linear Components: [%f, %f, %f]"%(msg.linear.x, msg.linear.y, msg.linear.z))
-    rospy.loginfo("Angular Components: [%f, %f, %f]"%(msg.angular.x, msg.angular.y, msg.angular.z))
+    global original_linx, previous_linx, current_linx, turn, move_x, move_y, \
+    move_z, angle, f_left, f_right, back_wheels
+
+    #rospy.loginfo("Received a /cmd_vel message!")
+    #rospy.loginfo("Linear Components: [%f, %f, %f]"%(msg.linear.x, msg.linear.y, msg.linear.z))
+    #rospy.loginfo("Angular Components: [%f, %f, %f]"%(msg.angular.x, msg.angular.y, msg.angular.z))
 
 
 
+    # a change in the linear speed has been detected
+    if not abs(msg.linear.x) == abs(current_linx):
+        previous_linx = current_linx
+        current_linx = msg.linear.x
+
+    elif not abs(msg.angular.z) == abs(turn): # else, only a change in angular speed has been detected
+        turn = msg.angular.z / original_linx
+        angle = msg.angular.z
 
 
-    # update joint_state
-
-    joint_state.header.stamp = rospy.Time.now()
-    joint_state.name = [
-        "front_axle_to_base" , "back_axle_to_base" ,
-        "front_left_wheel_to_front_axle" , "front_right_wheel_to_front_axle" ,
-        "back_left_wheel_to_back_axle" , "back_right_wheel_to_back_axle"
-    ]
-
-
-    f_left = 0
-    f_right = 0
-    back_wheels = 0
 
     if msg.linear.y == 0: # move
 
@@ -89,126 +102,96 @@ def receive_twist(msg):
                 back_wheels -= 3.14*msg.angular.z
 
 
-        joint_state.position = [ 0, 0, f_left, f_right, back_wheels, back_wheels ]
 
 
 
-    # for now, the following section is just dummy code
+    # strafe code needs to be added
 
     else: # strafe
+        return
 
-        if msg.linear.x > 0: # if forward
 
-            if msg.angular.z > 0: #
-                joint_state.position = [ 0.785, 0.785, 0.785, 0.785, 3.14*msg.angular.z, 3.14*msg.angular.z]
-
-            elif msg.angular.z == 0: #
-                joint_state.position = [ 0, 0, 0, 0, 0, 0 ]
-
-            else: # o
-                joint_state.position = [ -0.383, -0.383, -0.383, -0.383, -3.14*msg.angular.z, -3.14*msg.angular.z]
-
-        elif msg.linear.x == 0: # if angle
-
-            if msg.angular.z > 0: #
-                joint_state.position = [ 0.785, 0.785, 0.785, 0.785, 3.14*msg.angular.z, 3.14*msg.angular.z]
-
-            elif msg.angular.z == 0: #
-                joint_state.position = [ 0, 0, 0, 0, 0, 0 ]
-
-            else: # o
-                joint_state.position = [ -0.383, -0.383, -0.383, -0.383, -3.14*msg.angular.z, -3.14*msg.angular.z]
-
-        else: # if reverse
-
-            if msg.angular.z > 0: #
-                joint_state.position = [ 0.785, 0.785, 0.785, 0.785, 3.14*msg.angular.z, 3.14*msg.angular.z]
-
-            elif msg.angular.z == 0: #
-                joint_state.position = [ 0, 0, 0, 0, 0, 0 ]
-
-            else: #
-                joint_state.position = [ -0.383, -0.383, -0.383, -0.383, -3.14*msg.angular.z, -3.14*msg.angular.z]
-
-    # end of dummy code section
-
-    move_x = 0
-    move_y = 0
-    move_z = 0
+    move_x = msg.linear.x
+    move_y = msg.linear.y
+    move_z = msg.linear.z
 
 
 
-    # update transform and pose
-    quaternion = tf.transformations.quaternion_from_euler(0, 0, msg.angular.z)
 
-
-    trans.header.stamp = rospy.Time.now()
-    trans.header.frame_id = "joint_states"
-    trans.child_frame_id = joint_state.name
-    trans.transform.translation.x += msg.linear.x
-    trans.transform.translation.y += msg.linear.y
-    trans.transform.translation.z += msg.linear.z
-    trans.transform.rotation = quaternion
-
-    """
-    pose_st.header.stamp = rospy.Time.now()
-    pose_st.header.frame_id = "world"
-    pose_st.pose.position.x += msg.linear.y
-    pose_st.pose.position.y += msg.linear.x
-    pose_st.pose.position.z += msg.linear.z
-    pose_st.pose.orientation.x += quaternion[0]
-    pose_st.pose.orientation.y += quaternion[1]
-    pose_st.pose.orientation.z += quaternion[2]
-    pose_st.pose.orientation.w += quaternion[3]
-    """
+    move_joints()
+    move_robot()
 
 
 
+def move_joints():
+    # update joint_state
+
+
+    joint_state.header.stamp = rospy.Time.now()
+    joint_state.name = [
+        "front_axle_to_base" , "back_axle_to_base" ,
+        "front_left_wheel_to_front_axle" , "front_right_wheel_to_front_axle" ,
+        "back_left_wheel_to_back_axle" , "back_right_wheel_to_back_axle"
+    ]
+    joint_state.position = [ 0, 0, f_left, f_right, back_wheels, back_wheels ]
 
     # send the joint state and transform
     joint_pub.publish(joint_state)
-    #pose_pub.publish(pose_st)
 
-    """
+
+
+def move_robot():
+
+
+    # update transform and pose
+    quaternion = tf.transformations.quaternion_from_euler(0, 0, angle)
+
+
+
+    trans.header.stamp = rospy.Time.now()
+    trans.header.frame_id = "odom"
+    trans.child_frame_id = "base_link"
+    trans.transform.translation.x += move_x*quaternion[2]
+    trans.transform.translation.y += move_y*quaternion[3]
+    trans.transform.translation.z += move_z
+    trans.transform.rotation = quaternion
+
+
+
     broadcaster.sendTransform(
-        (msg.linear.x, msg.linear.y, msg.linear.z),
+        (trans.transform.translation.x, trans.transform.translation.y, trans.transform.translation.z),
         trans.transform.rotation,
         rospy.Time.now(),
         trans.header.frame_id,
         trans.child_frame_id
     )
-    broadcaster.sendTransform(
-        (msg.linear.x, msg.linear.y, msg.angular.z),
-        (pose_st.pose.orientation.x, pose_st.pose.orientation.y, pose_st.pose.orientation.z, pose_st.pose.orientation.w),
-        rospy.Time.now(),
-        "base_link",
-        "world"
-    )
-    """
+
+
+    rospy.loginfo("Angle: %f"%(angle))
+    rospy.loginfo("Quaternion: [%f, %f, %f, %f]"%(quaternion[0], quaternion[1], quaternion[2] , quaternion[3]))
 
 
 
 def listener():
-    rospy.init_node("joint_state_publisher")
     rospy.Subscriber("/cmd_vel", Twist, receive_twist)
-
     rospy.spin()
 
+
+
 if __name__ == '__main__':
+    rospy.init_node("car_publisher")
+    rospy.set_param("publish_default_positions", True)
+
     joint_state = JointState()
     joint_state.header = Header()
+
 
     trans = TransformStamped()
     trans.header = Header()
 
-    """
-    pose_st = PoseStamped()
-    pose_st.header = Header()
-    """
 
     broadcaster = tf.TransformBroadcaster()
     joint_pub = rospy.Publisher("joint_states", JointState, queue_size=1)
-    #pose_pub = rospy.Publisher("robot_pose", PoseStamped, queue_size=1)
 
     try:
         listener()
