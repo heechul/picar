ee1070a950afe27338643cd92b4a529d5a21c30c
Heechul Yun <heechul@illinois.edu>
2017-07-05 12:48:03 +0000

    take the recorded video/keyboard

---
+++ take.sh
@@ -0,0 +1,15 @@
+if [ ! -f "epoch_id.txt" ]; then
+    echo 0 > epoch_id.txt
+fi
+
+if [ ! -d "epochs" ]; then
+    mkdir epochs
+fi
+
+epoch_id=`cat epoch_id.txt`
+
+cp -v out-video.avi epochs/out-video-${epoch_id}.avi
+cp -v out-key.csv epochs/out-key-${epoch_id}.csv
+
+epoch_id=`expr ${epoch_id} + 1`
+echo ${epoch_id} > epoch_id.txt
