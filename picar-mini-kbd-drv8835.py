517e99a623229c456d51a0e334de3b100cec5c79
Heechul Yun <heechul@illinois.edu>
2017-07-10 17:38:02 -0500

    indentation fix

--- picar-mini-kbd-drv8835.py
+++ picar-mini-kbd-drv8835.py
@@ -26,32 +26,34 @@ rec_start_time = 0
 cur_speed = MAX_SPEED/2
 
 def stop():
-        global cur_speed
-        cur_speed = 0
-        motors.motor2.setSpeed(cur_speed)
+    global cur_speed
+    cur_speed = 0
+    motors.motor2.setSpeed(cur_speed)
         
 def ffw():
-        global cur_speed
-        cur_speed = min(MAX_SPEED, cur_speed + MAX_SPEED/10)
-        motors.motor2.setSpeed(cur_speed)
+    global cur_speed
+    # cur_speed = min(MAX_SPEED, cur_speed + MAX_SPEED/10)
+    cur_speed = MAX_SPEED-30
+    motors.motor2.setSpeed(cur_speed)
 def rew():
-        global cur_speed        
-        cur_speed = max(-MAX_SPEED, cur_speed - MAX_SPEED/10)
-        motors.motor2.setSpeed(cur_speed)
+    global cur_speed        
+    # cur_speed = max(-MAX_SPEED, cur_speed - MAX_SPEED/10)
+    cur_speed = -(MAX_SPEED-20)
+    motors.motor2.setSpeed(cur_speed)
 
 def center():
-        motors.motor1.setSpeed(0)
+    motors.motor1.setSpeed(0)
 def left():
-        motors.motor1.setSpeed(MAX_SPEED)
+    motors.motor1.setSpeed(MAX_SPEED)
 def right():
-        motors.motor1.setSpeed(-MAX_SPEED)
-
+    motors.motor1.setSpeed(-MAX_SPEED)
+        
 def degree2rad(deg):
-        return deg * math.pi / 180.0
+    return deg * math.pi / 180.0
 
 def turnOff():
-        stop()
-        center()
+    stop()
+    center()
 
 atexit.register(turnOff)
 
@@ -64,73 +66,73 @@ angle = 0.0
 btn   = ''
 
 while (True):
-        # read a frame
-        ret, frame = cap.read()
-        ts = int(time.time() * 1000000)
+    # read a frame
+    ret, frame = cap.read()
+    ts = int(time.time() * 1000000)
+    
+    if view_video == True:
+        cv2.imshow('frame', frame)
 
-        if view_video == True:
-                cv2.imshow('frame', frame)
+    ch = cv2.waitKey(50) & 0xFF
 
-        ch = cv2.waitKey(50) & 0xFF
-
-        if ch == ord('j'):
-                left()
-                print "left"
-                angle = degree2rad(-30)
-                btn   = ch
-        elif ch == ord('k'):
-                center()
-                print "center"
-                angle = degree2rad(0)
-                btn   = ch                
-        elif ch == ord('l'):
-                right()
-                print "right"
-                angle = degree2rad(30)
-                btn   = ch                
-        elif ch == ord('a'):
-                ffw()
-                print "accel"
-        elif ch == ord('s'):
-                stop()
-                print "stop"                
-                btn   = ch
-        elif ch == ord('z'):
-                rew()
-                print "reverse"
-        elif ch == ord('q'):
-                break
-	elif ch == ord('r'):
-                print "toggle record mode"
-                if rec_start_time == 0:
-                        rec_start_time = ts
-                else:
-                        rec_start_time = 0
-        elif ch == ord('t'):
-                print "toggle video mode"
-                if view_video == False:
-                        view_video = True
-                else:
-                        view_video = False
-		
+    if ch == ord('j'):
+        left()
+        print "left"
+        angle = degree2rad(-30)
+        btn   = ch
+    elif ch == ord('k'):
+        center()
+        print "center"
+        angle = degree2rad(0)
+        btn   = ch                
+    elif ch == ord('l'):
+        right()
+        print "right"
+        angle = degree2rad(30)
+        btn   = ch                
+    elif ch == ord('a'):
+        ffw()
+        print "accel"
+    elif ch == ord('s'):
+        stop()
+        print "stop"                
+        btn   = ch
+    elif ch == ord('z'):
+        rew()
+        print "reverse"
+    elif ch == ord('q'):
+        break
+    elif ch == ord('r'):
+        print "toggle record mode"
+        if rec_start_time == 0:
+            rec_start_time = ts
+        else:
+            rec_start_time = 0
+    elif ch == ord('t'):
+        print "toggle video mode"
+        if view_video == False:
+            view_video = True
+        else:
+            view_video = False
+	    
                         
-        if rec_start_time > 0:
-                # increase frame_id
-                frame_id = frame_id + 1
-                
-                # write input (angle)
-                str = "{},{},{}\n".format(ts, frame_id, angle)
-                keyfile.write(str)
-
-                # write input (button: left, center, stop, speed)
-                str = "{},{},{},{}\n".format(ts, frame_id, btn, cur_speed)
-                keyfile_btn.write(str)
-                
-                # write video stream
-                vidfile.write(frame)
-
-	        print ts, frame_id, angle, btn
-
+    if rec_start_time > 0:
+        # increase frame_id
+        frame_id = frame_id + 1
+        
+        # write input (angle)
+        str = "{},{},{}\n".format(ts, frame_id, angle)
+        keyfile.write(str)
+        
+        # write input (button: left, center, stop, speed)
+        str = "{},{},{},{}\n".format(ts, frame_id, btn, cur_speed)
+        keyfile_btn.write(str)
+        
+        # write video stream
+        vidfile.write(frame)
+        
+	print ts, frame_id, angle, btn
+        
 cap.release()
 keyfile.close()
 keyfile_btn.close()
