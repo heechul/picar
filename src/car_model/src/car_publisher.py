164801c85c0324c228ab6f7f60d020608573b960
James Muoghalu <jmuoghal@ittc.ku.edu>
2017-07-12 10:39:41 -0500

    Car_Publisher Testing

--- src/car_model/src/car_publisher.py
+++ src/car_model/src/car_publisher.py
@@ -20,36 +20,22 @@ class Car_Publisher:
         self.joint_pub = rospy.Publisher("joint_states", JointState, queue_size=50)
         self.odom_pub = rospy.Publisher("odom", Odometry, queue_size=50)
         self.broadcaster = tf.TransformBroadcaster()
-
-        self.f_left = 0; self.f_right = 0; self.back_wheels = 0;
-        self.original_speed = 0; self.current_linx = 0.5; self.turn = 1;
-        self.angle = (math.pi/2); self.previous_angle = (math.pi/2);
-        self.first_message = False; self.turn_left = False; self.turn_right = False;
-        self.move_x = 0; self.move_y = 0; self.move_z = 0;
-        self.vx = 0.1; self.vy = 0.1; self.vth = self.angle;
-        self.current_time = rospy.Time.now(); self.last_time = rospy.Time.now();
+        self.thr = Thread(target = self.publish_messages)
 
 
-
-
-    def initialize_joint_states(self):
         self.joint_state.header = Header()
         self.joint_state.header.frame_id = "base_footprint"
         self.joint_state.header.stamp = rospy.Time.now()
-
         self.joint_state.name = [
             "base_footprint_joint" , "camera_joint" ,
             "front_axle_to_base" , "back_axle_to_base" ,
             "front_left_wheel_to_front_axle" , "front_right_wheel_to_front_axle" ,
             "back_left_wheel_to_back_axle" , "back_right_wheel_to_back_axle"
         ]
-
         self.joint_state.position = [0, 0, 0, 0, 0, 0, 0, 0]
 
-
-    def initialize_transformation(self):
         quat_1 = tf.transformations.quaternion_from_euler(0,0,0)
-        quat_2 = tf.transformations.quaternion_from_euler(0,0,math.pi/2)
+        quat_2 = tf.transformations.quaternion_from_euler(0,0,(math.pi/2))
 
         self.trans.header = Header()
         self.trans.header.frame_id = "odom"
@@ -60,18 +46,26 @@ class Car_Publisher:
         self.trans.transform.translation.z = 0
         self.trans.transform.rotation = quat_1
 
+        self.odom.header = Header()
         self.odom.header.frame_id = "odom";
         self.odom.child_frame_id = "base_footprint"
         self.odom.header.stamp = rospy.Time.now()
         self.odom.pose.pose.position.x = 0
         self.odom.pose.pose.position.y = 0
         self.odom.pose.pose.position.z = 0
-        self.odom.pose.pose.orientation = quat_2
+        self.odom.pose.pose.orientation.x = quat_2[0]
+        self.odom.pose.pose.orientation.y = quat_2[1]
+        self.odom.pose.pose.orientation.z = quat_2[2]
+        self.odom.pose.pose.orientation.w = quat_2[3]
         self.odom.twist.twist = Twist(Vector3(0,0,0), Vector3(0,0,0))
 
-
-
-
+        self.f_left = 0; self.f_right = 0; self.back_wheels = 0;
+        self.original_speed = 0.5; self.current_linx = 0.5; self.turn = 1;
+        self.angle = (math.pi/2); self.previous_angle = (math.pi/2);
+        self.first_message = False; self.turn_left = False; self.turn_right = False;
+        self.move_x = 0; self.move_y = 0; self.move_z = 0;
+        self.vx = 0.1; self.vy = 0.1; self.vth = self.angle;
+        self.current_time = rospy.Time.now(); self.last_time = rospy.Time.now();
 
 
 
@@ -135,7 +129,7 @@ class Car_Publisher:
                     self.angle += (math.pi/72)
 
 
-
+        # variable for comparing angles?
         while self.angle < 0:
             self.angle += 2*math.pi
         while self.angle > 2*math.pi:
@@ -152,7 +146,8 @@ class Car_Publisher:
         self.move_y = msg.linear.x * self.vy * math.sin(self.angle)
         self.move_z = msg.linear.z
 
-        self.joint_state = [0, 0, 0, 0, self.f_left, self.f_right, self.back_wheels, self.back_wheels]
+        self.joint_state.position = [0, 0, 0, 0, self.f_left, self.f_right, self.back_wheels, self.back_wheels]
+
 
 
 
@@ -160,11 +155,11 @@ class Car_Publisher:
 
     def publish_messages(self):
 
-        while True:
+        while not rospy.is_shutdown():
 
-            if self.first_message and not (self.f_left != 0):
+            if self.first_message and not (self.f_left == 0):
 
-                if self.angle == 0 or self.angle == (M_PI/2) or self.angle == (M_PI*1.5):
+                if self.angle == 0 or self.angle == (math.pi/2) or self.angle == (math.pi*1.5):
                     self.turn_left = False; self.turn_right = False;
 
                 else:
@@ -173,24 +168,27 @@ class Car_Publisher:
 
                     self.previous_angle = self.angle;
                     if self.turn_left:
-                        self.angle += (M_PI/72)
+                        self.angle += (math.pi/72)
                     else:
-                        self.angle -= (M_PI/72);
+                        self.angle -= (math.pi/72);
 
 
-                self.move_x = self.move_x * cos(self.angle) / math.cos(self.previous_angle)
-                self.move_y = self.move_y * sin(self.angle) / math.sin(self.previous_angle)
-
-                # self.joint_state.position[0] = self.angle
+                self.move_x = self.move_x * math.cos(self.angle) / math.cos(self.previous_angle)
+                self.move_y = self.move_y * math.sin(self.angle) / math.sin(self.previous_angle)
 
             quat_1 = tf.transformations.quaternion_from_euler(0,0, (self.angle - math.pi/2) )
             quat_2 = tf.transformations.quaternion_from_euler(0,0,self.angle)
 
             self.last_time = self.current_time; self.current_time = rospy.Time.now()
 
+            self.joint_state.header = Header()
+            self.joint_state.header.frame_id = "base_footprint"
             self.joint_state.header.stamp = self.current_time
             self.joint_pub.publish(self.joint_state)
 
+            self.trans.header = Header()
+            self.trans.header.frame_id = "odom"
+            self.trans.child_frame_id = "base_footprint"
             self.trans.header.stamp = self.current_time;
             self.trans.transform.translation.x += self.move_x
             self.trans.transform.translation.y += self.move_y
@@ -200,39 +198,49 @@ class Car_Publisher:
                 (self.trans.transform.translation.x, self.trans.transform.translation.y, self.trans.transform.translation.z),
                 self.trans.transform.rotation,
                 self.current_time,
-                self.trans.header.frame_id,
-                self.trans.child_frame_id
+                self.trans.child_frame_id,
+                self.trans.header.frame_id
             )
 
+            self.odom.header = Header()
+            self.odom.header.frame_id = "odom";
+            self.odom.child_frame_id = "base_footprint"
             self.odom.header.stamp = self.current_time
             self.odom.pose.pose.position.x += self.move_x
             self.odom.pose.pose.position.y += self.move_y
             self.odom.pose.pose.position.z += self.move_z
-            self.odom.pose.pose.orientation = quat_2
+            self.odom.pose.pose.orientation.x = quat_2[0]
+            self.odom.pose.pose.orientation.y = quat_2[1]
+            self.odom.pose.pose.orientation.z = quat_2[2]
+            self.odom.pose.pose.orientation.w = quat_2[3]
             self.odom_pub.publish(self.odom);
 
+            """
+            rospy.loginfo("Angle: %f"%(self.angle))
+            rospy.loginfo("Move: [%f , %f]"%(self.move_x, self.move_y))
+            rospy.loginfo("Transform: [%f, %f, %f]"%(
+                self.trans.transform.translation.x, self.trans.transform.translation.y,
+                self.trans.transform.translation.z)
+            )
+            """
             sleep(0.1)
+
 
 
 
     def listener(self):
         rospy.Subscriber("cmd_vel", Twist, self.receive_twist)
+        self.thr.start()
         rospy.spin()
 
 
 
 
-
 if __name__ == '__main__':
-    rospy.init_node("car_publisher")
+    rospy.init_node("car_publisher", anonymous=True)
     car_pub = Car_Publisher()
-    car_pub.initialize_joint_states(); car_pub.initialize_transformation();
-    thr = Thread(target = car_pub.publish_messages)
 
     try:
-        thr.start()
-        thr.join()
         car_pub.listener()
     except rospy.ROSInterruptException:
-
         pass
