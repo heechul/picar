corrected btn model accuracy calculation

model_btn.py
@@ -94,5 +94,5 @@ b_fc5 = bias_variable([3])
 # y = tf.multiply(tf.atan(tf.matmul(h_fc4_drop, W_fc5) + b_fc5), 2) #scale the atan output
 
 y_lg  = tf.matmul(h_fc4_drop, W_fc5) + b_fc5
-# y     = tf.nn.softmax(y_lg)
+y     = tf.nn.softmax(y_lg)
 

train_btn.py
@@ -80,7 +80,7 @@ for i in xrange(params.training_steps):
 # Let's see if we can predict
 vxx, vyy = data.load_batch('val')
 
-prediction = tf.argmax(model.y_lg, 1) + 106
+prediction = tf.argmax(model.y_lg, 1) 
 correct_prediction = tf.equal(prediction, tf.argmax(model.y_oh_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 