5c309b4c56263e51ea33525dc792f5ea90b34b41
Heechul Yun <heechul@illinois.edu>
2017-08-28 22:17:55 -0500

    rc based control

---
+++ arduino/rc_flysky.ino
@@ -0,0 +1,22 @@
+int ch1; // Here's where we'll keep our channel values
+int ch3;
+
+void setup()
+{
+  pinMode(4, INPUT); 
+  pinMode(3, INPUT);
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  ch1 = pulseIn(4, HIGH, 25000); // Read the pulse width of 
+  ch3 = pulseIn(3, HIGH, 25000);
+
+  Serial.print(ch1);
+  Serial.print(' ');
+  Serial.print(ch3);
+  Serial.print('\n');
+  
+  delay(50);
+}
