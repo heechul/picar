1216e1b4b2c6b3408f8a61c91b2f13162c2077ef
Heechul Yun <heechul@illinois.edu>
2017-09-04 18:58:53 -0500

    output ch 1,3,5

--- arduino/rc_flysky.ino
+++ arduino/rc_flysky.ino
@@ -1,10 +1,12 @@
 int ch1; // Here's where we'll keep our channel values
 int ch3;
+int ch5;
 
 void setup()
 {
   pinMode(4, INPUT); 
   pinMode(3, INPUT);
+  pinMode(5, INPUT);
   Serial.begin(115200);
 }
 
@@ -14,12 +16,15 @@ void loop()
     String str = Serial.readStringUntil('\n');
 
     if (str == "getrc" ) {
-      ch1 = pulseIn(4, HIGH, 25000); // Read the pulse width of 
-      ch3 = pulseIn(3, HIGH, 25000);
-
+      ch1 = pulseIn(4, HIGH, 25000); // right left/right stick
+      ch3 = pulseIn(3, HIGH, 25000); // left up/down stick
+      ch5 = pulseIn(5, HIGH, 25000); // SWA 
+  
       Serial.print(ch1);
       Serial.print(' ');
       Serial.print(ch3);
+      Serial.print(' ');
+      Serial.print(ch5);
       Serial.print('\n');
     }
   }
