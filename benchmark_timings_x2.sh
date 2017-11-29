c7e538a2ca6d633d557b3d8e024fe7f11556534e
Elise <elisemmc@gmail.com>
2017-11-29 12:09:25 -0600

    updates TX2 script for perf

--- benchmark_timings_x2.sh
+++ benchmark_timings_x2.sh
@@ -13,14 +13,14 @@ echo "The output files can be found in: logs/test-model/$identification"
 
 echo 2-parallel-single-core
 ./bandwidth -a write -m 16384 -t 10000 -c 3 &
-taskset -c 0 python test-model.py 1 > logs/test-model-and-benchmark/$identification/1-sc-bench_1-sc-model_cpu0.txt
+perf stat -o logs/test-model-and-benchmark/$identification/1-sc-bench_1-sc-model_cpu0_perf.txt -e cache-misses,cache-references,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses taskset -c 0 python test-model.py 1 > logs/test-model-and-benchmark/$identification/1-sc-bench_1-sc-model_cpu0.txt
 jobs -p | xargs kill
 echo end 2-parallel-single-core
 
 echo 3-parallel-single-core
 ./bandwidth -a write -m 16384 -t 10000 -c 3 &
 ./bandwidth -a write -m 16384 -t 10000 -c 4 &
-taskset -c 0 python test-model.py 1 > logs/test-model-and-benchmark/$identification/2-sc-bench_1-sc-model_cpu0.txt
+perf stat -o logs/test-model-and-benchmark/$identification/2-sc-bench_1-sc-model_cpu0_perf.txt -e cache-misses,cache-references,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses taskset -c 0 python test-model.py 1 > logs/test-model-and-benchmark/$identification/2-sc-bench_1-sc-model_cpu0.txt
 jobs -p | xargs kill
 echo end 3-parallel-single-core
 
@@ -28,25 +28,25 @@ echo 4-parallel-single-core
 ./bandwidth -a write -m 16384 -t 10000 -c 3 &
 ./bandwidth -a write -m 16384 -t 10000 -c 4 &
 ./bandwidth -a write -m 16384 -t 10000 -c 5 &
-taskset -c 0 python test-model.py 1 > logs/test-model-and-benchmark/$identification/3-sc-bench_1-sc-model_cpu0.txt
+perf stat -o logs/test-model-and-benchmark/$identification/3-sc-bench_1-sc-model_cpu0_perf.txt -e cache-misses,cache-references,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses taskset -c 0 python test-model.py 1 > logs/test-model-and-benchmark/$identification/3-sc-bench_1-sc-model_cpu0.txt
 echo end 4-parallel-single-core
 
 echo 1-single-core_1-dual-core
 ./bandwidth -a write -m 16384 -t 10000 -c 4 &
-taskset -c 0,3 python test-model.py 2 > logs/test-model-and-benchmark/$identification/1-sc-bench_1-dc-model_cpu03.txt
+perf stat -o logs/test-model-and-benchmark/$identification/1-sc-bench_1-dc-model_cpu03_perf.txt -e cache-misses,cache-references,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses taskset -c 0,3 python test-model.py 2 > logs/test-model-and-benchmark/$identification/1-sc-bench_1-dc-model_cpu03.txt
 jobs -p | xargs kill
 echo end 2-parallel-dual-core
 
 echo 2-parallel-dual-core
 ./bandwidth -a write -m 16384 -t 10000 -c 4 &
 ./bandwidth -a write -m 16384 -t 10000 -c 5 &
-taskset -c 0,3 python test-model.py 2 > logs/test-model-and-benchmark/$identification/2-sc-bench_1-dc-model_cpu03.txt
+perf stat -o logs/test-model-and-benchmark/$identification/2-sc-bench_1-dc-model_cpu03_perf.txt -e cache-misses,cache-references,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses taskset -c 0,3 python test-model.py 2 > logs/test-model-and-benchmark/$identification/2-sc-bench_1-dc-model_cpu03.txt
 jobs -p | xargs kill
 echo end 2-parallel-dual-core
 
 echo tri-core
 ./bandwidth -a write -m 16384 -t 10000 -c 5 &
-taskset -c 0,3,4 python test-model.py 3 > logs/test-model-and-benchmark/$identification/1-sc-bench_1-tc-model_cpu034.txt
+perf stat -o logs/test-model-and-benchmark/$identification/1-sc-bench_1-tc-model_cpu034_perf.txt -e cache-misses,cache-references,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses taskset -c 0,3,4 python test-model.py 3 > logs/test-model-and-benchmark/$identification/1-sc-bench_1-tc-model_cpu034.txt
 jobs -p | xargs kill
 echo end tri-core
 
