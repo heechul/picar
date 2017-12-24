5cdc67160ba9fde03995b78338c810b823694bef
Heechul Yun <heechul@illinois.edu>
2017-12-24 17:39:53 -0600

    RC signal interrupt support.

--- arduino-rc-to-arduino.py
+++ arduino-rc-to-arduino.py
@@ -27,14 +27,16 @@ while (True):
         time.sleep(next(g))
         ts = time.time()        
         ser.write("getrc\n")
+        ts_start = time.time()
         line = ser.readline().rstrip("\n\r")
-        print "line:", line
+        ts_end   = time.time()
+        print "line:{0} {1} ms".format(line, (ts_end - ts_start)*1000)
         rc_inputs = line.split(' ')
         if len(rc_inputs) != 2:
                 continue
         if int(rc_inputs[0]) == 0 or int(rc_inputs[1]) == 0:
                 continue # there must be a timeout
-        print "rc_str: {0}, rc_thr: {1}".format(rc_inputs[0], rc_inputs[1])
+        print "rc_thr: {0}, rc_str: {1}".format(rc_inputs[0], rc_inputs[1])
         
         # steering [0], throttle [1]
         cmd = "setpwm {0} {1}\n".format(rc_inputs[0], rc_inputs[1])
--- arduino/rc_donkey.ino
+++ arduino/rc_donkey.ino
@@ -1,42 +1,86 @@
 #include <Servo.h>
 
+const int throttle_intr_pin = 3;
+const int steering_intr_pin = 2;
+
+const int steering_servo_pin = 9;
+const int throttle_servo_pin = 10;
+
 Servo steer, throttle;
-int ch1, ch2;
+
+volatile int ch1_pulse_us, ch2_pulse_us;   // duty cycles (in us)
+volatile int ch1_prev_time, ch2_prev_time; // temporal store for time
+
+void isr_rc_ch1_rising()
+{
+  ch1_prev_time = micros();
+  attachInterrupt(digitalPinToInterrupt(throttle_intr_pin),
+                  isr_rc_ch1_falling, FALLING);
+}
+
+void isr_rc_ch1_falling()
+{
+  int dur = micros() - ch1_prev_time;
+  if (dur > 0) /* !overflow */
+    ch1_pulse_us = dur;
+  attachInterrupt(digitalPinToInterrupt(throttle_intr_pin),
+                  isr_rc_ch1_rising, RISING);    
+}
+
+void isr_rc_ch2_rising()
+{
+  ch2_prev_time = micros();
+  attachInterrupt(digitalPinToInterrupt(steering_intr_pin),
+                  isr_rc_ch2_falling, FALLING);
+}
+
+void isr_rc_ch2_falling()
+{
+  int dur = micros() - ch2_prev_time;
+  if (dur > 0) /* !overflow */
+    ch2_pulse_us = dur;
+  attachInterrupt(digitalPinToInterrupt(steering_intr_pin),
+                  isr_rc_ch2_rising, RISING);    
+}
 
 void setup()
 {
-	// RC input pins
-	pinMode(2, INPUT); 
-	pinMode(3, INPUT);
+  pinMode(steering_intr_pin, INPUT_PULLUP);
+  pinMode(throttle_intr_pin, INPUT_PULLUP);
+  
+  // RC input pins
+  attachInterrupt(digitalPinToInterrupt(throttle_intr_pin),
+                  isr_rc_ch1_rising, RISING);
+  attachInterrupt(digitalPinToInterrupt(steering_intr_pin),
+                  isr_rc_ch2_rising, RISING);
+  
+  // servo control pins
+  steer.attach(steering_servo_pin);
+  throttle.attach(throttle_servo_pin);
 
-	// servo control pins
-	steer.attach(9);
-	throttle.attach(10);
-
-	// serial init
-	Serial.begin(115200);
-
-	// initialize servo values at 1500us
-	ch1 = ch2 = 1500; 
+  // serial init
+  Serial.begin(115200);
+  
+  // initialize servo values at 1500us
+  ch1_pulse_us = ch2_pulse_us = 1500; 
 }
 
 void loop()
 {
-	if(Serial.available() > 0) {
-		String str = Serial.readStringUntil('\n');
-		char *c_str = str.c_str();
+  if(Serial.available() > 0) {
+    String str = Serial.readStringUntil('\n');
+    char *c_str = str.c_str();
 		
-		if (!strncmp(c_str, "getrc", 5)) {
-			ch1 = pulseIn(2, HIGH, 50000); // steering (25ms timeout)
-			ch2 = pulseIn(3, HIGH, 50000); // throttle (25ms timeout)
-			Serial.print(ch1);
-			Serial.print(' ');
-			Serial.print(ch2);
-			Serial.print('\n');
+    if (!strncmp(c_str, "getrc", 5)) {
+      Serial.print(ch1_pulse_us);
+      Serial.print(' ');
+      Serial.print(ch2_pulse_us);
+      Serial.print('\n');
 		} else if (!strncmp(c_str, "setpwm", 6)) {
-			sscanf(c_str + 7, "%d %d", &ch1, &ch2);
-			steer.writeMicroseconds(ch1);
-			throttle.writeMicroseconds(ch2);
+      int ch1, ch2;
+      sscanf(c_str + 7, "%d %d", &ch1, &ch2);
+      steer.writeMicroseconds(ch1);
+      throttle.writeMicroseconds(ch2);
 		}
 	}
 }
