ad211de4e63045f8bc28204875c844cf27ee8492
Heechul Yun <heechul@illinois.edu>
2017-12-28 15:50:44 -0600

    ..

--- params.py
+++ params.py
@@ -5,8 +5,10 @@ import os
 from collections import OrderedDict
 
 batch_size = 255
-# save_dir = os.path.abspath('models')
-save_dir = os.path.abspath('../picar/models-donkey')
+
+VER="-donkey-T78910_V1112"
+save_dir = os.path.abspath('../picar/models' + VER)
+
 training_steps = 2000
 img_height = 66
 img_width = 200
@@ -16,7 +18,7 @@ use_category_normal = False # if ture, center/curve images are equally selected.
 
 # change this to the directory that contains the source videos
 data_dir = os.path.abspath('../picar/epochs')
-out_dir = os.path.abspath('../picar/output')
+out_dir = os.path.abspath('../picar/output' + VER)
 
 # data_dir = os.path.abspath('epochs')
 # out_dir = os.path.abspath('.output')
@@ -28,50 +30,7 @@ if not os.path.isdir(out_dir):
     os.makedirs(out_dir)
 
 epochs = OrderedDict()
-# 8/23 picar-v2.0, 50ms fix
 
-epochs['train'] = [3,4] 
-epochs['val'] = [4]
-
-#epochs['train'] += [414, 415, 416, 417, 419, 420, 421, 422]
-#epochs['val'] += [418, 423]
-
-# 8/21 picar-v2.0
-# left = 302 - 306
-# right = 307 - 311
-# epochs['train'] = [302, 303, 304, 305, 307, 308, 309, 310]
-# epochs['val'] = [306, 311] 
-
-# center following.
-# left  = 120-135 (18)
-# right = 117-119, 136 - 144, 100-101 (14)
-# epochs['train'] = [100, 101, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144]
-# epochs['val'] =   [145, 146]
-
-
-# # right only
-# epochs['train'] = [100, 101, 110, 111, 117, 118]
-# epochs['val'] =   [112, 119]
-
-# right & left (120-124)
-# epochs['train'] = [100, 101, 110, 111, 117, 118, 120, 121, 122, 123]
-# epochs['val'] =   [112, 119, 124]
-
-# epochs['train'] = [100, 102, 104, 106, 108, 110, 112, 114, 103, 105, 107, 109, 113, 115] 
-# epochs['val'] =   [101, 111]
-
-# step   10 of 2000, train loss 0.177199780941,   val loss 0.183928459883
-# step 1000 of 2000, train loss 0.00693200016394, val loss 0.0959444642067
-# step 2000 of 2000, train loss 0.00296380673535, val loss 0.0754969492555
-
-
-# epochs['train'] = [100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115] 
-# epochs['val'] = [100] 
-
-# epochs['train'] = [6, 7, 8, 14, 15, 16, 18, 19, 20, 22] 
-# epochs['val'] = [13, 17, 21] 
-# epochs['train'] = [18,19,20] # range(0,5)
-# epochs['val'] = [21] # range(0,5)
-# epochs['train'] = [3, 4, 5, 6, 8]
-# epochs['val'] = [1, 2, 7, 9, 10]
+epochs['train'] = [7,8,9,10]
+epochs['val'] = [11,12]
 
