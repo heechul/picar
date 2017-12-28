8f0f6f59b56590a377cd8cf4c4cf680f0570a4e4
Heechul Yun <heechul@illinois.edu>
2017-12-28 16:12:59 -0600

    stat name conflict.

--- mystat.py
+++ mystat.py
@@ -1,6 +1,12 @@
+import os
+import time
+import numpy as np
+from PIL import Image
+import glob
+
 # stats
 class DeadlineStat:
-    def __init__(self, deadline):
+    def __init__(self, deadline=0):
         self.deadline = deadline
         self.deadline_miss_cnt = 0
         self.exectime_min = 0 # min exec. time
@@ -8,7 +14,7 @@ class DeadlineStat:
         self.exectime_sum = 0 # sum exec. times
         self.exectime_cnt = 0 # cnt exec. times
 
-    def update(exec_time):
+    def update(self, exec_time):
         self.exectime_cnt += 1
         self.exectime_sum += exec_time
         self.exectime_min = min(self.exectime_min, exec_time)
@@ -16,13 +22,15 @@ class DeadlineStat:
         if exec_time > self.deadline:
             self.deadline_miss_cnt += 1
             
-    def print_stat():
+    def print_stat(self):
         print "Print statistics"
         print "================"
-        print "min/avg/max: {:.2f} {:.2f} {:.2f}".format(
-            self.exectime_min,
-            self.exectime_tot/self.exectime_cnt,
-            self.exectime_max)
+        print "min/avg/max (ms): {:.2f} {:.2f} {:.2f}".format(
+            self.exectime_min * 1000,
+            self.exectime_sum/self.exectime_cnt * 1000,
+            self.exectime_max * 1000)
         print "deadline_miss_cnt: {}".format(self.deadline_miss_cnt)
+        
+
         
 
