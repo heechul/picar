90ab38d7ca5803f4bb11b3421eda73c9f78dfea6
Heechul Yun <heechul.yun@gmail.com>
2018-01-03 15:18:04 -0600

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-roscore &
-sleep 5
-roslaunch rtimulib_ros rtimulib_ros.launch &  # publish 'imu' channel
-rosrun teleop_twist_joy teleop_node &         # publish 'cmd_vel' output from joystick input
-./servod &              # enable pwm output for the speed motor
-./src/motor_driver.py & # subscribe 'cmd_vel' channel
-# rosrun teleop_twist_keyboard teleop_twist_keyboard.py &
