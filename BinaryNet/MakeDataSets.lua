706250cf997c5e62a16cae0a5308fe205e1a7f1e
mbechtel2 <mbechtel@ku.edu>
2018-03-19 10:22:07 -0500

    Change datasets to store frames with channels first

--- BinaryNet/MakeDataSets.lua
+++ BinaryNet/MakeDataSets.lua
@@ -22,8 +22,12 @@ local val_set =
 
 local trainEpochs = table.getn(params.train_sets)
 
+print("Training Data:")
+
 for i = 1,trainEpochs do
   local epochNum = params.train_sets[i]
+
+  print("    Getting data from epoch "..epochNum)
 
   local csvpath = paths.concat("epochs", "out-key-"..epochNum..".csv")
   file = io.open(csvpath, "r")
@@ -34,7 +38,7 @@ for i = 1,trainEpochs do
   for line in io.lines(csvpath) do
     if firstLine ~= true then
       local col = ParseCSVLine(line, ",")
-      train_set.angles[#train_set.angles+1] = col[3]
+      train_set.angles[#train_set.angles+1] = tonumber(col[3])
     else
       firstLine = false
     end
@@ -46,15 +50,22 @@ for i = 1,trainEpochs do
   local ret, frame = vid:read{}
 
   while ret do
-    train_set.frames[#train_set.frames+1] = frame
+    frame = cv.resize{frame, {params.img_width, params.img_height}}
+    cv.imwrite{'temp.png', frame}
+    img = image.load('temp.png', 3, 'byte')
+    train_set.frames[#train_set.frames+1] = img
     ret = vid:read{frame}
   end
 end
 
 local valEpochs = table.getn(params.val_sets)
 
+print("Validation Data:")
+
 for i = 1,valEpochs do
   local epochNum = params.val_sets[i]
+
+  print("    Getting data from epoch "..epochNum)
 
   local csvpath = paths.concat("epochs", "out-key-"..epochNum..".csv")
   file = io.open(csvpath, "r")
@@ -65,7 +76,7 @@ for i = 1,valEpochs do
   for line in io.lines(csvpath) do
     if firstLine ~= true then
       local col = ParseCSVLine(line, ",")
-      val_set.angles[#val_set.angles+1] = col[3]
+      val_set.angles[#val_set.angles+1] = tonumber(col[3])
     else
       firstLine = false
     end
@@ -77,13 +88,19 @@ for i = 1,valEpochs do
   local ret, frame = vid:read{}
 
   while ret do
-    val_set.frames[#val_set.frames+1] = frame
+    frame = cv.resize{frame, {params.img_width, params.img_height}}
+    cv.imwrite{'temp.png', frame}
+    img = image.load('temp.png', 3, 'byte')
+    val_set.frames[#val_set.frames+1] = img
     ret = vid:read{frame}
   end
 end
 
 assert(table.getn(train_set.frames) == table.getn(train_set.angles), "Unequal number of frames and angles")
 assert(table.getn(val_set.frames) == table.getn(val_set.angles), "Unequal number of frames and angles")
+
+print("# of Training frames/angles = "..table.getn(train_set.frames))
+print("# of Validation frames/angles = "..table.getn(val_set.frames))
 
 local train_file = paths.concat("datasets", "train.t7")
 torch.save(train_file, train_set)
